b91f2e1839ef00d4a0dd32b977d9a53ce3cc497d
Fangdun Cai (Fundon) <cfddream@gmail.com>
2016-07-16 13:23:26 +0800

    ProgressTracker

---
+++ src/ProgressTracker.vue
@@ -0,0 +1,17 @@
+<template>
+  <ul class="progress-tracker">
+  </ul>
+</template>
+
+<script>
+import StepItem from './StepItem'
+
+export default {
+  components: {
+    StepItem
+  }
+}
+</script>
+
+<style lang="scss">
+</style>
---
+++ src/StepItem.vue
@@ -0,0 +1,17 @@
+<template>
+  <li class="progress-step" :class="{ 'is-active': isActived, 'is-complete': isCompleted }">
+    <span class="progress-marker"></span>
+    <span class="progress-text">
+      <h4 class="progress-title"></h4>
+    </span>
+  </li>
+</template>
+
+<script>
+export default {
+  props: {
+    isActived: Boolean,
+    isCompleted: Boolean
+  }
+}
+</script>
